8fe445bb127585727db8d5e1d462fb20dcbc6932
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-09-22 22:28:38 +0000

    Solution to this kaya. Hooray

---
+++ Greed is Good 5kyu Kata/index.js
@@ -0,0 +1,86 @@
+function score(dice) {
+  // Fill me in!
+  // const rules = {
+  //   'ThreeOnes': 1000,
+  //   ""
+
+  // }
+  // const sortedDice = dice.sort((a, b) => a - b);
+  const occurrences = dice.reduce(function (acc, curr) {
+    return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
+  }, {});
+  console.log(occurrences);
+
+  let score = [];
+  const entries = Object.entries(occurrences);
+
+  for (let [key, value] of entries) {
+    if (key == 1) {
+      if (value == 3) {
+        score.push(1000);
+      }
+      if (value == 4) {
+        score.push(1100);
+      }
+      if (value == 1) {
+        score.push(100);
+      }
+      if (value == 2) {
+        score.push(200);
+      }
+    } else {
+      score.push(0);
+    }
+    if (key == 2) {
+      if (value == 3) {
+        score.push(200);
+      }
+    } else {
+      score.push(0);
+    }
+    if (key == 3) {
+      if (value == 3) {
+        score.push(300);
+      }
+      if (value == 5) {
+        score.push(300);
+      }
+    } else {
+      score.push(0);
+    }
+    if (key == 4) {
+      if (value == 3) {
+        score.push(400);
+      }
+    } else {
+      score.push(0);
+    }
+    if (key == 5) {
+      if (value == 3) {
+        score.push(500);
+      }
+      if (value == 4) {
+        score.push(550);
+      }
+      if (value == 1) {
+        score.push(50);
+      }
+    } else {
+      score.push(0);
+    }
+    if (key == 6) {
+      if (value == 3) {
+        score.push(600);
+      }
+    } else {
+      score.push(0);
+    }
+  }
+
+  const totalScore = score.reduce((acc, cur) => acc + cur, 0);
+  console.log(totalScore);
+
+  // for (let { i, j } in occurences) {
+  // }
+}
+score([3, 1, 3, 1, 2]);
